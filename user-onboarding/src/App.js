1efaf575490aaa8ad543154f8d6a86f856885609
cFoschini <cjfoschini25@gmail.com>
2021-06-08 23:36:50 -0400

    unable to finsih due to npm intall not installing

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -2,26 +2,54 @@ import './App.css';
 import Form from "./Form";
 import React, { useState } from "react";
 
+  
+const initialFormValues = {
+  name: "",
+  email: "",
+  password: "",
+  terms: false
+};
+
+const initialFormErrors = {
+  name: "",
+  email: "",
+  password: "",
+  terms: ""
+}
+
+const initialDisabled = true;
+
 
 function App() {
 
-  const initialFormValues = {
-    name: "",
-    email: "",
-    password: "",
-    terms: false
-  };
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
 
-  const [formValues, setFormValues] = useState(initialFormValues)
+  const inputChange = setFormValues({ 
+    ...formValues,
+    [name]: value
+  });
+
+  const formSubmit = () => {
+    const newUser = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      terms: formValues.name.trim()
+    }
+  }
+
 
   return (
     <div className="App">
       <Form
-      name=""
-      email=""
-      password=""
-      terms=""
-      />
+      values={formValues}
+      change={inputChange}
+      submit={formSubmit}
+      disabled={disabled}
+      errors={formErrors}
+       />
     </div>
   );
 }
--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,11 +1,19 @@
 export default function Form(props) {
-    const { name, email, password, terms } = props;
+    const {
+        values,
+        submit,
+        change,
+        disabled,
+        errors,
+      } = props
     return (
         <div>
             <form>
-                <input type="text" name="name" value="props.name"/>
-                <input type="text" name="email" value="props.email"/>
-                <input type="text" name="password" value="props.password"/>
+                <input type="text" name="name" value={values.name}/>
+                <input type="text" name="email" value={values.email}/>
+                <input type="text" name="password" value={values.password}/>
+                <input type="checkbox" name="terms" checked={values.terms}/>
+                <button>Create new user</button>
             </form>
         </div>
     )
